f3979121d0f8f6444489c4b3f53beede296a4fea
lum <lum@lumetas.lumetas.ru>
2026-05-13 21:51:50 +0500

    wrapper path fix

--- src/DumpAllKeys.php
+++ src/DumpAllKeys.php
@@ -2,13 +2,15 @@
 
 namespace Lugit;
 
+use BMND\DI;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $keys = new KeysRepository();
 
 $keyString = "";
 foreach ($keys->yieldKeys() as $key => $username) {
-	$keyString .= "command=\"php /var/lugit/src/SshWrapper.php '$username'\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding $key\n";
+	$keyString .= "command=\"php " . escapeshellarg(__DIR__/SshWrapper.php) . " '$username'\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding $key\n";
 }
 
 file_put_contents(getenv('HOME') . '/.ssh/authorized_keys', $keyString);
