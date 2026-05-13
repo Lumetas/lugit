a466e40c1d41b479d0b3253b8348d13d946fdf61
lum <lum@lumetas.lumetas.ru>
2026-05-13 16:03:22 +0500

    Удалён самодельный парсер Markdown исправлен баг с страницей профиля

--- src/Controllers/WebController.php
+++ src/Controllers/WebController.php
@@ -1,23 +1,32 @@
 <?php
+
 namespace Lugit\Controllers;
 
-
+use BMND\Http\Request;
+use BMND\Router\Error;
 use BMND\Router\Route;
 use Lugit\RepoPage;
 
-class WebController {
+class WebController
+{
 	#[Route('/')]
-	public function dashboard() :void {
+	public function dashboard(): void
+	{
 		readfile(__DIR__ . '/../static/index.html');
 	}
 
 	#[Route('/{user}/{repo}')]
-	public function repo(string $user, string $repo, RepoPage $page) :void {
+	#[Route('/{user}/{repo}/')]
+	public function repo(string $user, string $repo, RepoPage $page): void
+	{
 		$page->handle($user, $repo);
 	}
 
 	#[Route('/{user}')]
-	public function user(string $user, RepoPage $page) :void {
+	#[Route('/{user}/')]
+	public function user(string $user, RepoPage $page): void
+	{
 		$page->handleUserProfile($user);
 	}
+
 }
