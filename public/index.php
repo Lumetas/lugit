aafea199cb7f29f2ae89f26635a514ae35518b37
lum <lum@lumetas.lumetas.ru>
2026-05-13 15:39:40 +0500

    update bmnd add cache

--- public/index.php
+++ public/index.php
@@ -1,48 +1,8 @@
 <?php
 namespace Lugit;
 require_once __DIR__ . '/../vendor/autoload.php';
-
+use Lugit\Config;
 use BMND\Router\Router;
 
-Router::setup('../src/Controllers', 'Lugit\\Controllers\\', '..')->run();
 
-exit;
-
-$path = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH);
-
-if ($path === '/' || $path === '') {
-	readfile(__DIR__ . '/../src/static/index.html');
-} else if ($path == '/_script.js') {
-	header('Content-Type: text/javascript');
-    readfile(__DIR__ . '/../src/static/script.js');
-} else if ($path == '/_style.css') {
-	header('Content-Type: text/css');
-    readfile(__DIR__ . '/../src/static/style.css');
-} elseif (str_starts_with($path, '/api/')) {
-    $api = new GitApi();
-    $api->handle();
-} elseif (preg_match('#^/?([^/]+)/([^/]+)/?$#', $path, $matches)) {
-    $username = $matches[1];
-    $repoName = $matches[2];
-    
-    if (str_contains($path, '/info/') || str_contains($path, '/git-upload') || str_contains($path, '/git-receive')) {
-        $server = new GitHttpServer();
-        $server->handle();
-    } else {
-        $page = new RepoPage();
-        $page->handle();
-    }
-} elseif (preg_match('#^/?([^/]+)/?$#', $path, $matches)) {
-    $username = $matches[1];
-    
-    if (str_contains($path, '/info/') || str_contains($path, '/git-')) {
-        $server = new GitHttpServer();
-        $server->handle();
-    } else {
-        $page = new RepoPage();
-        $page->handleUserProfile($username);
-    }
-} else {
-    $server = new GitHttpServer();
-    $server->handle();
-}
+Router::setup('../src/Controllers', 'Lugit\\Controllers\\', '..', Config::get('routesCache', true))->run();
